bf21c0f641c189b8461996153e89af7c4d7ab033
dlacewell <dlacewell@gmail.com>
2015-12-03 21:52:13 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@
 
 from setuptools import setup
 
-
-install_requires = filter(lambda d: not d.startswith('-e'), open("requirements.txt", "r").readlines())
-
 setup(
     name='plait',
     version='0.2.0',
@@ -13,7 +10,16 @@ setup(
     author_email='dlacewell@gmail.com',
     url='https://github.com/dustinlacewell/plait',
     packages=['plait', 'plait.app'],
-    install_requires=install_requires + ['twisted'],
+    install_requires=[
+        'structlog',
+        'click',
+        'twisted',
+        'pycrypto',
+        'pyasn1',
+        'blessings',
+        'blinker',
+        'urwid',
+    ],
     dependency_links = ['https://github.com/twisted/twisted/tarball/trunk#egg=twisted'],
     entry_points={
         'console_scripts': [
